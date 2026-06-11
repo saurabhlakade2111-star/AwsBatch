a6fca4800c8b21ca2a1d575b5d3118a18c261acf
Ganesh Lakade <Ganesh.Lakade@INFOVISIONLABS.COM>
2026-06-11 16:20:33 +0530

    updated main class

--- src/main/java/com/example/awsbatch/AwsBatchApplication.java
+++ src/main/java/com/example/awsbatch/AwsBatchApplication.java
@@ -1,13 +1,28 @@
 package com.example.awsbatch;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AwsBatchApplication {
+public class AwsBatchApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(AwsBatchApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println("Batch Job Started");
+
+        for(int i=1;i<=10;i++) {
+            System.out.println("Processing Record " + i);
+            Thread.sleep(1000);
+        }
+
+        System.out.println("Batch Job Completed");
+
+        System.exit(0);
+    }
 }
